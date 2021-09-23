87a6c6223c09977669725ed873a16063e23da8d9
Bakaryc50 <bakaryc50@gmail.com>
2021-09-23 07:22:19 -0700

    Bakary commit fixed: monty/op_push.c

---
+++ op_push.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * op_push - push an element onto the stack
+ * @stack: stack pointer
+ */
+void op_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	const char *nstr = op_env.argv[1];
+
+	if (!(nstr && isinteger(nstr)))
+		pfailure("L%u: usage: push integer\n", line_number);
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		pfailure("Error: malloc failed\n");
+
+	new->n = atoi(nstr);
+	if (*stack)
+	{
+		new->prev = (*stack);
+		new->next = (*stack)->next;
+		new->next->prev = new;
+		(*stack)->next = new;
+		if (op_env.mode == LIFO)
+			(*stack) = new;
+	}
+	else
+	{
+		new->prev = new;
+		new->next = new;
+		(*stack) = new;
+	}
+}
