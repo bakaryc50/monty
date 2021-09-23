eee6620179810cb843fdc6998bb1862e59d09e99
Bakaryc50 <bakaryc50@gmail.com>
2021-09-23 07:18:56 -0700

    Bakary commit fixed some def: monty/ monty.h

--- monty.h
+++ monty.h
@@ -1,6 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -30,5 +35,64 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * enum stack_mode_n - stack mode enumeration
+ * @LIFO: operate as a stack
+ * @FIFO: operate as a queue
+ */
+typedef enum stack_mode_n
+{
+	LIFO = 0,
+	FIFO = 1
+} stack_mode_t;
+
+/**
+ * struct op_env_s - operation environment
+ * @sp: top of the stack
+ * @argv: argument vector
+ * @line: line buffer
+ * @linesz: line buffer size
+ * @lineno: line number
+ * @mode: stack operation mode
+ */
+typedef struct op_env_s
+{
+	stack_t *stack;
+	char **argv;
+	char *line;
+	size_t linesz;
+	stack_mode_t mode;
+} op_env_t;
+
+extern op_env_t op_env;
+
+void (*get_instruction_fn)(const char *opcode, unsigned int line_number);
+
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t **stack);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
+void op_pchar(stack_t **stack, unsigned int line_number);
+void op_pstr(stack_t **stack, unsigned int line_number);
+void op_rotr(stack_t **stack, unsigned int line_number);
+void op_rotl(stack_t **stack, unsigned int line_number);
+
+char **tokenize(char *str);
+size_t count_tokens(const char *str);
+
+void free_op_env(void);
+
+void pfailure(const char *fmt, ...);
+
+int isinteger(const char *str);
 
 #endif /* end of MONTY_H */
